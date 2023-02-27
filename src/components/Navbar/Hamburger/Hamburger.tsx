e689487735fcffa4f8a99cbf1a3270db94897cc2
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-02-27 22:33:43 +0200

    ADD line layer @ index.css

--- src/components/Navbar/Hamburger/Hamburger.tsx
+++ src/components/Navbar/Hamburger/Hamburger.tsx
@@ -3,9 +3,9 @@ import IHamburger from "./Hamburger.interface";
 const Hamburger = ({ toggleValue, toggleFunction }: IHamburger) => {
     return (
         <div className="w-12 h-6 flex flex-col justify-between items-center select-none cursor-pointer lg:hidden" onClick={toggleFunction}>
-            <span className={`line-toggler transition-1\/2 ${toggleValue ? "rotate-left-45" : ""}`}></span>
-            <span className={`line-toggler-half transition-1\/2 ${toggleValue ? "scale-hide" : ""}`}></span>
-            <span className={`line-toggler transition-1\/2 ${toggleValue ? "rotate-right-45" : ""}`}></span>
+            <span className={`line line-toggler-full transition-1/2 ${toggleValue ? "rotate-left-45" : ""}`}></span>
+            <span className={`line line-toggler-half transition-1/2 ${toggleValue ? "scale-hide" : ""}`}></span>
+            <span className={`line line-toggler-full transition-1/2 ${toggleValue ? "rotate-right-45" : ""}`}></span>
         </div>
     );
 };
