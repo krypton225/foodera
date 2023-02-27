7bd0f90029f15d2e552819ce7c9903a2d74e605c
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-02-27 20:02:12 +0200

    EDIT padding @ navbar & button in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,18 +3,18 @@ import listDataItems from "./list-data";
 
 const Navbar = () => {
     return (
-        <nav className="w-full">
+        <nav className="w-full py-6">
             <div className="container flex justify-between items-center">
                 <div className="w-44">
                     <img src={Logo} alt="Foodera" />
                 </div>
 
-                <ul className="flex">
+                <ul className="flex items-center">
                     {listDataItems.map(({ id, linkText, linkURL, isButton }) => {
                         return (
-                            <li key={id} className="mx-4">
+                            <li key={id} className="mx-6">
                                 <a href={linkURL}
-                                    className={`block p-4 capitalize font-semibold text-lg text-primary-dark ${isButton ? "custom-btn" : ""}`}>
+                                    className={`block py-[0.5rem] capitalize font-semibold text-lg text-primary-dark ${isButton ? "custom-btn" : ""}`}>
                                     {linkText}
                                 </a>
                             </li>
