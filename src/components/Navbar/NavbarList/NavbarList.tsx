78427861caa33519a5417e5929297d971b4f911d
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-02-27 22:52:02 +0200

    EDIT width of navbar in responsive screens

--- src/components/Navbar/NavbarList/NavbarList.tsx
+++ src/components/Navbar/NavbarList/NavbarList.tsx
@@ -4,9 +4,9 @@ import INavbarList from "./NavbarList.interface";
 
 const NavbarList = ({ togglerValue }: INavbarList) => {
     return (
-        <ul className={`w-[90%] h-auto py-8 absolute ${togglerValue ? "top-28 opacity-100 visible" : "top-16 opacity-0 invisible"} right-[5%] 
-                rounded-md flex flex-col justify-center items-center bg-primary-light lg:static lg:py-0 lg:bg-transparent 
-                lg:flex lg:flex-row lg:items-center lg:justify-end lg:h-auto lg:visible lg:opacity-100 transition-all duration-[0.66s]`}>
+        <ul className={`max-[639px]:w-[95%] sm:w-[90%] md:w-[80%] h-auto py-8 absolute ${togglerValue ? "top-28 opacity-100 visible" : "top-16 opacity-0 invisible"} 
+        max-[639px]:right-[2%] sm:right-[5%] md:right-[10%] rounded-md flex flex-col justify-center items-center bg-primary-light lg:static lg:py-0 lg:bg-transparent 
+                lg:flex lg:flex-row lg:items-center lg:justify-end lg:h-auto lg:visible lg:opacity-100 transition-all duration-[0.66s] z-[9999]`}>
 
             {listDataItems.map(({ id, linkText, linkURL, isButton }) => {
                 return (
