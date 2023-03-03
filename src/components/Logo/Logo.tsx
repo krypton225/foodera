b6a434b32478ec25f5b6b8f50a0606f99b1eb24e
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-03-04 00:12:55 +0200

    ADD eslint comment for href @ Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,6 +5,7 @@ import { LogoImage } from "../../data/Photos";
 const Logo = ({ widthInRem = 44 }: ILogo) => {
     return (
         <div className={`w-32 lg:w-${widthInRem}`} >
+            {/* eslint-disable-next-line */}
             <a href="#" className="w-full">
                 <img src={LogoImage} alt="Foodera" className="w-full h-full object-cover" />
             </a>
