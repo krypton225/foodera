efa8ab2f7d77684954a98ba3f89705a1dbe965f8
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-03-04 00:02:44 +0200

    ADD anchor tag @ Logo img

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,9 @@ import { LogoImage } from "../../data/Photos";
 const Logo = ({ widthInRem = 44 }: ILogo) => {
     return (
         <div className={`w-32 lg:w-${widthInRem}`} >
-            <img src={LogoImage} alt="Foodera" className="w-full h-full object-cover" />
+            <a href="#" className="w-full">
+                <img src={LogoImage} alt="Foodera" className="w-full h-full object-cover" />
+            </a>
         </div >
     );
 };
