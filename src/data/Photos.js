de61ffb31b0e83615b2354d00bc6dc58638ee42b
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-03-05 22:27:39 +0200

    DEL pics @ assets & ADD same in webp format

--- src/data/Photos.js
+++ src/data/Photos.js
@@ -1,18 +1,18 @@
-export { default as LogoImage } from "../assets/images/logo.png";
+export { default as LogoImage } from "../assets/images/logo.webp";
 
-export { default as LandingImageBg } from "../assets/images/landing-bg.jpg";
+export { default as LandingImageBg } from "../assets/images/landing-bg.webp";
 
-export { default as Features1 } from "../assets/images/features/features-1.png";
-export { default as Features2 } from "../assets/images/features/features-2.png";
+export { default as Features1 } from "../assets/images/features/features-1.webp";
+export { default as Features2 } from "../assets/images/features/features-2.webp";
 
-export { default as Food1 } from "../assets/images/food/foods-1.jpg";
-export { default as Food2 } from "../assets/images/food/foods-2.jpg";
-export { default as Food3 } from "../assets/images/food/foods-3.jpg";
+export { default as Food1 } from "../assets/images/food/foods-1.webp";
+export { default as Food2 } from "../assets/images/food/foods-2.webp";
+export { default as Food3 } from "../assets/images/food/foods-3.webp";
 
-export { default as Parallax1 } from "../assets/images/parallax/parallax-1.jpg";
-export { default as Parallax2 } from "../assets/images/parallax/parallax-2.jpg";
+export { default as Parallax1 } from "../assets/images/parallax/parallax-1.webp";
+export { default as Parallax2 } from "../assets/images/parallax/parallax-2.webp";
 
-export { default as Testimonials1 } from "../assets/images/testimonials/testimonials-1.jpg";
-export { default as Testimonials2 } from "../assets/images/testimonials/testimonials-2.jpg";
-export { default as Testimonials3 } from "../assets/images/testimonials/testimonials-3.jpg";
-export { default as Testimonials4 } from "../assets/images/testimonials/testimonials-4.jpg";
+export { default as Testimonials1 } from "../assets/images/testimonials/testimonials-1.webp";
+export { default as Testimonials2 } from "../assets/images/testimonials/testimonials-2.webp";
+export { default as Testimonials3 } from "../assets/images/testimonials/testimonials-3.webp";
+export { default as Testimonials4 } from "../assets/images/testimonials/testimonials-4.webp";
