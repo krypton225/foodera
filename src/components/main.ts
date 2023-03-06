64324df294321199b525a5e123dc338fe7bb9bb5
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-06 21:53:46 +0200

    UPD main file @ component fldr & Its content

---
+++ src/components/main.ts
@@ -0,0 +1,27 @@
+export { default as Navbar } from "./Navbar/Navbar";
+
+export { default as Logo } from "./Logo/Logo";
+
+export { default as Landing } from "./Landing/Landing";
+
+export { default as MainButton } from "./MainButton/MainButton";
+
+export { default as LandingState } from "./LandingState/LandingState";
+
+export { default as About } from "./About/About";
+
+export { default as WatchStory } from "./WatchStory/WatchStory";
+
+export { default as TitleSection } from "./TitleSection/TitleSection";
+
+export { default as Explore } from "./Explore/Explore";
+
+export { default as Testimonials } from "./Testimonials/Testimonials";
+
+export { default as FAQ } from "./FAQ/FAQ";
+
+export { default as FreshDaily } from "./FreshDaily/FreshDaily";
+
+export { default as Subscribe } from "./Subscribe/Subscribe";
+
+export { default as Footer } from "./Footer/Footer";
